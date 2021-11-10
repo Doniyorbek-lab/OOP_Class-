3c9ef6382798d996774bc7057c757e972854d4c1
Doniyorbek-lab <93943647+Doniyorbek-lab@users.noreply.github.com>
2021-11-10 16:48:30 +0500

    my first c# project

---
+++ OOP_Class/OOP_Class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = GetStudent();
+            Print(student);
+        }
+        static void Print(Student student)
+        {
+            Console.WriteLine(student.firstName);
+            Console.WriteLine(student.lastName);
+            Console.WriteLine(student.age);
+            Console.WriteLine(student.Id);
+            Console.WriteLine(student.favouriteColor);
+        }
+        static Student GetStudent()
+        {
+            Student student = new Student();
+
+            student.age = 15;
+            student.firstName = "Xakimbekov";
+            student.lastName = "Doniyorbek";
+            student.Id = Guid.NewGuid();
+            student.favouriteColor = Color.Orange.ToString();
+            return student;
+        }
+    }
+}
---
+++ OOP_Class/OOP_Class/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class
+{
+     class Student
+    {
+        public Guid Id;
+        public string firstName;
+        public string lastName;
+        public int age;
+        public string favouriteColor;
+    }
+}
